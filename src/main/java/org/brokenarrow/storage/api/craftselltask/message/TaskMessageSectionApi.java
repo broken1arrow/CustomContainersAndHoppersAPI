81b7385fba261a02d69175f95b9f9edd177cb421
broken1arrow <broken2arrow1@gmail.com>
2025-08-23 21:00:43 +0200

    Fix the api logic for sound

--- src/main/java/org/brokenarrow/storage/api/craftselltask/message/TaskMessageSectionApi.java
+++ src/main/java/org/brokenarrow/storage/api/craftselltask/message/TaskMessageSectionApi.java
@@ -1,16 +1,13 @@
 package org.brokenarrow.storage.api.craftselltask.message;
 
 import org.brokenarrow.storage.api.craftselltask.TaskDataApi;
-import org.brokenarrow.storage.api.craftselltask.TaskProvider;
 import org.brokenarrow.storage.api.craftselltask.util.TypeOfTask;
 
 import java.util.function.Consumer;
 
 public interface TaskMessageSectionApi {
 
-    TaskMessageSectionApi body(Consumer<TaskProvider> body);
-
-    TaskDataApi getTaskData();
+    TaskMessageSectionApi body(Consumer<TaskDataApi> body);
 
     TypeOfTask getTaskType();
 }
